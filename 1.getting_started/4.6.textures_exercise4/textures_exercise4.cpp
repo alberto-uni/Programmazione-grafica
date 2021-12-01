9d1f1be051b89638672a51a86aae3a22635ff714
alberto-uni <64833476+alberto-uni@users.noreply.github.com>
2021-12-01 09:07:20 +0100

    Add files via upload

--- 1.getting_started/4.6.textures_exercise4/textures_exercise4.cpp
+++ 1.getting_started/4.6.textures_exercise4/textures_exercise4.cpp
@@ -206,13 +206,13 @@ void processInput(GLFWwindow* window)
 
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
     {
-        mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        mixValue += 0.005f; // change this value accordingly (might be too slow or too fast based on system hardware)
         if (mixValue >= 1.0f)
             mixValue = 1.0f;
     }
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
     {
-        mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        mixValue -= 0.005f; // change this value accordingly (might be too slow or too fast based on system hardware)
         if (mixValue <= 0.0f)
             mixValue = 0.0f;
     }
